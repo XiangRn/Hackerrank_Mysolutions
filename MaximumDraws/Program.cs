aa7b22615639ef6645f8b32b073549636adcf821
XiangRn <cuadao1710@gmail.com>
2021-01-28 17:57:41 +0700

    Just added some file from HK

---
+++ MaximumDraws/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumDraws
+{
+    class Program
+    {
+        static int maximumDraws(int n)
+        {
+            return n + 1;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int result = maximumDraws(n);
+
+                Console.WriteLine(result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReverseGame/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseGame
+{
+    class Program
+    {
+        static int Reverse_Game(int N, int K)
+        {
+            return K < N / 2 ? 2 * K + 1 : 2 * (N - K - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int t = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int j = 0; j < t; j++)
+            {
+                string[] nk = Console.ReadLine().Split(' ');
+                int n = Convert.ToInt32(nk[0]);
+
+                int k = Convert.ToInt32(nk[1]);
+                Console.WriteLine(Reverse_Game(n, k));
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
