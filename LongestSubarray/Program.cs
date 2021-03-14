7007a451a2906328f433770c0568a82c9191123e
XiangRn <cuadao1710@gmail.com>
2021-03-14 19:04:40 +0700

    Just add some files

---
+++ LongestSubarray/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestSubarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;        
+            int max = 0;
+            List<int> ls = new List<int>() { 1,1,3,3,2,2,7,7,6,6,6,8,8 };
+            List<int> ls2 = new List<int>();
+                     int i=0;int j;
+          while(ls.Count>0&&i<ls.Count-1)
+            {
+                ls2.Add(ls[i]);
+                for ( j = i+1; j <ls.Count; j++)
+                {
+                    if (Math.Abs(ls[i] - ls[j]) == 1)
+                    {
+                        if (ls2.Count < 2)
+                        {
+                            ls2.Add(ls[j]);
+                            if (j == ls.Count - 1)
+                            {
+                                max = Math.Max(max, (j - i + 1));
+                                break;
+                            } continue;
+                        }
+                        if(ls2.Count==2 && ls2.Contains(ls[j]))
+                        {
+                            if (j == ls.Count - 1)
+                            {
+                                max = Math.Max(max, (j - i+1));
+                                break;
+                            } continue;
+                        }
+                        else
+                        {
+                            max = Math.Max(max, (j - i));
+                            break;
+                        }
+                    }
+                    if(Math.Abs(ls[i] - ls[j]) == 0)
+                    {
+                        continue;
+                    }
+                   if(Math.Abs(ls[i] - ls[j]) > 1)
+                    {
+                        if (ls2.Count == 2)
+                        {
+                            max = Math.Max(max, (j - i));
+                        } break;
+                    }    
+                }          
+                ls2.Clear();
+                i++;
+            }
+ 
+            Console.WriteLine(max);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Parralel/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parralel
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'minTime' function below.
+         *
+         * The function is expected to return a LONG_INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY files
+         *  2. INTEGER numCores
+         *  3. INTEGER limit
+         */
+
+        public static long minTime(List<int> files, int numCores, int limit)
+        {
+            int temp = -1;
+           files.Sort();
+            files.Reverse();
+                       long sum = 0;
+          
+            while (files.Count > 0 && numCores>0)
+            {
+                if (limit > 0)
+                {
+                    if (files[0] % numCores == 0)
+                    {
+                        sum += (files[0] / numCores);
+                        limit--;
+                        files.RemoveAt(0);
+                        continue;
+                    }
+                }
+                 sum += files[0];
+               // Console.WriteLine("sum : "+sum);
+                files.RemoveAt(0);
+                   
+            }
+            Console.WriteLine("limit"+limit);
+            return sum;
+           
+         
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int filesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> files = new List<int>();
+
+            for (int i = 0; i < filesCount; i++)
+            {
+                int filesItem = Convert.ToInt32(Console.ReadLine().Trim());
+                files.Add(filesItem);
+            }
+
+            int numCores = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int limit = Convert.ToInt32(Console.ReadLine().Trim());
+            long result = Result.minTime(files, numCores, limit);
+            Console.Write("\n");
+         Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
