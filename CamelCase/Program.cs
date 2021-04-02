ea1ddcb18ea1ed1881fe747d64e3c5d2c12385d1
XiangRn <cuadao1710@gmail.com>
2021-04-03 00:49:00 +0700

    Add CamelCase exercise

---
+++ CamelCase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string s = Console.ReadLine();
+            List<char> s1 = s.ToList();
+            int i = 0; int count = 1;
+            while (s1.Count > 0 && i< s1.Count)
+            {
+                if (char.IsUpper(s1[i]))
+                {
+                    count++;
+                   
+                }
+                i++;
+                
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
