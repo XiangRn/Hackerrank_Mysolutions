f8bb6c3df33c9d25fd0e7479cb0d67037391def2
XiangRn <cuadao1710@gmail.com>
2021-06-17 16:28:19 +0700

    Add a file

---
+++ StringConstruction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringConstruction
+{
+    class Program
+    {
+        public static int stringConstruction(string s)
+        {
+            s = string.Join("", s.ToCharArray().Distinct());
+            return s.Length;
+        }
+        static void Main(string[] args)
+        {
+           // int n = Convert.ToInt32(Console.ReadLine());
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s = Console.ReadLine();
+
+                int result = stringConstruction(s);
+
+                Console.WriteLine(result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TheLoveLetterMystery/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLoveLetterMystery
+{
+    class Program
+    {
+        public static int theLoveLetterMystery(string s)
+        {
+            char[] arr = s.ToCharArray(); int sum = 0;
+            if (CheckPalindrome(s))
+            {
+                return 0;
+            }
+            else
+            {
+                for (int i = 0; i < arr.Length / 2; i++)
+                {
+                    if (arr[i] != arr[arr.Length - 1 - i])
+                    {
+                        if (arr[i].CompareTo(arr[arr.Length - 1 - i]) < 0)
+                        {
+                            sum += Math.Abs(arr[i].CompareTo(arr[arr.Length - 1 - i]));
+                            arr[arr.Length - 1 - i] = arr[i];
+
+                        }
+                        else
+                        {
+                            sum += Math.Abs(arr[i].CompareTo(arr[arr.Length - 1 - i]));
+                            arr[i] = arr[arr.Length - 1 - i];
+                        }
+                    }
+                }
+               return sum;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+          
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s = Console.ReadLine();
+
+                int result = theLoveLetterMystery(s);
+
+                Console.WriteLine(result);
+            }
+
+            Console.ReadLine();
+        }
+        private static bool CheckPalindrome(string s)
+        {
+            var middle = s.Length / 2;
+            int i = 0;
+            while (i < middle)
+            {
+                if (s[i] != s[s.Length - 1 - i])
+                {
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        }
+    }
+}
